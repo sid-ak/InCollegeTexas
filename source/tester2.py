f89ea9fae9d4df09f5b768f1878edc10ba8e69d9
anshika-bhowmick <anshikabhowmick5@gmail.com>
2022-09-17 20:33:36 -0400

    seperating the tests

--- source/tester2.py
+++ source/tester2.py
@@ -1,8 +1,6 @@
+from typing import List
 import pytest
 from authentication.Signup import ValidatePassword
-from loggedInActions.LearnNewSkill import DisplaySkills
-from loggedInActions.JobInternshipSearch import FindJobInternshipAction
-from loggedInActions.FindSomeone import FindSomeoneAction
 
 def test_ValidatePassword():
     good_pwd = ["Test123@", "Testing1234@"]
@@ -12,19 +10,3 @@ def test_ValidatePassword():
     bad_pwd = ["Test123", "test123@", "Test@One", "Test123@test1"]
     for pwd in bad_pwd:
         assert ValidatePassword(pwd) == False
-
-def test_LearnNewSkill(capfd):
-    skills = ['communication', 'marketing', 'python programming', 'web development', 'public speaking']
-    DisplaySkills(skills)
-    out, err = capfd.readouterr()
-    assert out == "1. Communication\n2. Marketing\n3. Python Programming\n4. Web Development\n5. Public Speaking\n"
-
-def test_JobInternshipSearch(capfd):
-    FindJobInternshipAction()
-    out, err = capfd.readouterr()
-    assert out == "\nunder construction\n"
-
-def test_FindSomeone(capfd):
-    FindSomeoneAction()
-    out, err = capfd.readouterr()
-    assert out == "\nunder construction\n"
