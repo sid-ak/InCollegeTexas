a4bbe3a7f9f37f908a661950e690c859f3ba7733
sid-ak <sidharthak25@gmail.com>
2022-09-22 22:48:14 -0400

    Revert "Merge pull request #14 from sid-ak/IN-12-search-user"
    This reverts commit 05413da92651e21e20e7b164afde83c53e19083f, reversing
    changes made to 540d9d537af4d3a29c4e8ced28074038871fec39.

--- source/authentication/Signup.py
+++ source/authentication/Signup.py
@@ -1,4 +1,4 @@
-from model.User import User, UserHelpers
+from firebaseSetup.Firebase import database
 
 # this function will validate the password and return True, if it is valid, False otherwise
 def ValidatePassword(password: str) -> bool:
@@ -28,12 +28,9 @@ def ValidatePassword(password: str) -> bool:
 def CheckDBSize() -> bool:
     try:
         # get all DB entries tp a local list
-        users = UserHelpers.GetAllUsers()
+        queryResults = database.child('Users').get()
 
-        if (users == None):
-            return True
-
-        if len(users) >= 5:
+        if len(queryResults.each()) >= 5:
             print("\nAll permitted accounts have been created, please come back later!")
             return False
         else:
@@ -43,38 +40,40 @@ def CheckDBSize() -> bool:
 
 
 # this function will accept username and password and return True, if the registration
-# was successful, False otherwise; it validates database size limits and uniqueness of username
-def RegisterNewUser() -> bool:
-    try:        
+# was sucessful, False othwerwise; it validates database size limits and uniqueness of username
+def RegisterNewUser(username: str, password: str) -> bool:
+    try:
         # first let's check that the total number of users does not exceed 5
+        # get all DB entries to a local list
+        queryResults = database.child('Users').get()
+
         if not CheckDBSize():
             return False
-        
-        username = input("\nPlease enter your username: ")
-        users = UserHelpers.GetAllUsers()
-        if (users != None):
-            # get all DB entries to a local list
+        else:
             # now check that the username is unique
-            for user in users:
-                if user.Username == username:
+            for query in queryResults.each():
+                if query.val()['username'] == username:
                     print("\nError! This username already exists!")
                     return False
     except:
         print("\nError! Something went wrong when connecting to database to fetch all entries!")
-        return False
+        return False    
     
     # now let's check the password to see if it abides by the set standards
-    password = input("\nPlease enter your password: ")
     if not ValidatePassword(password=password):
         print("\nError! Your password does not meet one or some of the standards!")
         return False 
-    
+
     # if the validation checks above pass, now we can try to create a new entry with the given values
-    firstName = input("\nPlease enter your first name: ")
-    lastName = input("\nPlease enter your last name: ")
     try:
-        userId = UserHelpers.CreateUserId(username, password)
-        UserHelpers.CreateUser(User(userId, username, firstName, lastName))
+        # save a new entry inside the "Users" node
+        database.child('Users').push(
+            {
+                "username": username, 
+                "password": password
+            }
+        )
+        
         return True
     except:
         print("\nError! Something went wrong when connecting to database to push a new entry!")
