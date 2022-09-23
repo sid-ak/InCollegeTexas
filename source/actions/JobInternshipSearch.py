7c35862fedc53d6a53ad0d664daa94e9c737f69c
sid-ak <sidharthak25@gmail.com>
2022-09-23 19:39:54 -0400

    IN-17: Introduced a Job model.

--- source/actions/JobInternshipSearch.py
+++ source/actions/JobInternshipSearch.py
@@ -1,4 +1,43 @@
 
 # this function is under construction
-def FindJobInternshipAction():
-    print("\nunder construction")
+from model.Job import Job, JobHelpers
+from model.User import User
+
+def FindJobInternshipAction(loggedUser: User) -> bool:
+    print("\nPlease enter one of the following options to continue:\n"
+        + "1 - Post a job")
+    optionNo: int = int(input("\nEnter (-1 to Exit): "))
+    
+    try:
+        if optionNo == -1:
+            # TODO: Implement go back functionality.
+            return False
+        
+        elif optionNo == 1:
+            job: Job = MakeJob()
+            raise Exception() if JobHelpers.CreateJob(job) == False else print(
+                f"{job.Title} created successfully."
+            )
+        
+        FindJobInternshipAction(loggedUser)
+    except:
+        print(f"{job.Title} could not be created.")
+        return False
+
+def MakeJob(jobPoster: User) -> Job:
+    print("\nPlease enter the following details for the job posting: ")
+    title: str = input("\nTitle: ")
+    employer: str = input("\nEmployer: ")
+    desc: str = input("\nDescription:\n")
+    loc: str = input("\n\nLocation: ")
+    salary: str = input("\nSalary: ")
+
+    id: str = JobHelpers.CreateJobId(
+        title,
+        employer,
+        desc,
+        loc,
+        salary
+    )
+
+    return Job(id, title, employer, desc, loc, salary, jobPoster)
