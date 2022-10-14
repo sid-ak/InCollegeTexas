f0dd35cf17a7ca35e5f348ef050ef1f1995cada7
obasit7 <basitosama@gmail.com>
2022-10-13 23:45:48 -0400

    added additional logic for accepting, rejecting and removing a user from friend list

--- source/model/User.py
+++ source/model/User.py
@@ -69,14 +69,14 @@ class UserHelpers:
 
     # takes User as parameter whose friends we need to find
     # returns a list of User (list[User]) of all of the userToFind's friends
-    def GetFriends(userToFind: str) -> list[User]:
+    def GetFriends(userNameToFind: str) -> list[User]:
         friends = []
         usersResponse = database.child("Users").get()
 
         for user in usersResponse.each():
             if user == None:
                 continue
-            elif (user.val()["Username"] == userToFind):
+            elif (user.val()["Username"] == userNameToFind):
                 try:
                     friends_dict = user.val()["Friends"]
                     if len(friends_dict) == 0:
@@ -173,15 +173,19 @@ class UserHelpers:
         except:
             print("Exception occurred. Targeted Advertising preference could not be toggled.")
 
+    # helper to check if a given user is present in the db
+    def CheckUserPrescenceInDB(user: User, collection: str = "Users") -> bool:
+        dbusers = UserHelpers.GetAllUsers(collection)
+        for user in dbusers:
+            if user.Username == user.Username:
+                return True
+        return False
+
     # Sends friend request from sender to receiver
     # adds senders username to receivers friends dictionary as pending(False)
     def SendFriendRequest(sender: User, receiver: User, collection: str = "Users") -> bool:
-        dbusers = UserHelpers.GetAllUsers()
-        isPresent = False
-        for user in dbusers:
-            if user.Username == receiver.Username:
-                isPresent = True
-                break
+
+        isPresent = UserHelpers.CheckUserPrescenceInDB(receiver, collection)
         if not isPresent:
             print("Receiving user is not a registered user. Friend request can't be sent")
             return False
@@ -193,7 +197,7 @@ class UserHelpers:
         try:
             receiver.Friends[sender.Username] = False
             new_dict = receiver.Friends
-            database.child("Users").child(receiver.Id).child("Friends").set(new_dict)
+            database.child(collection).child(receiver.Id).child("Friends").set(new_dict)
 
             print(f"\nFriend request sent to: {receiver.Username}")
             return True
@@ -201,5 +205,104 @@ class UserHelpers:
         except:
             print("\nException occurred. Friend request could not be sent\n")
             return False
+
+    # function accepts User class object as param
+    # and adds userToAdd user to their friends list
+    # and adds user to userToAdd's friend list (2-way update)
+    def AcceptFriendRequest(user: User, userToAdd: User, collection: str = "Users") -> bool:
+        # checkis user to add in DB
+        isPresent = UserHelpers.CheckUserPrescenceInDB(userToAdd, collection)
+        if not isPresent:
+            print("Receiving user is not a registered user. Friend request can't be sent")
+            return False
+        # check if userToAdd in users friend list
+        if userToAdd.Username not in user.Friends:
+            print(f"\nUh Oh! Something went wrong. {userToAdd.Username} couldn't be found\n")
+            return False
+        # check if userToAdd in users friend list and already accepted
+        elif user.Friends[userToAdd.Username] == True:
+            print(f"\nLooks like you're already friends with {userToAdd.Username}!\n")
+            return False
+
+        try:
+            # add userToAdds username to user's friends dict in db
+            user.Friends[userToAdd.Username] = True
+            new_dict = user.Friends
+            database.child(collection).child(user.Id).child("Friends").set(new_dict)
+            # add user's username to userToAdd's friend dict in db
+            userToAdd.Friends[user.Username] = True
+            new_dict = userToAdd.Friends
+            database.child(collection).child(userToAdd.Id).child("Friends").set(new_dict)
+
+            print(f"\nSuccess! {userToAdd.Username} has been added as your friend!\n")
+            return True
+
+        except:
+            print("\nOh No! Something went wrong while adding friends!\n")
+            return False
+
+    # rejecting a userToReject user by removing it from user User's Friend list
+    def RejectFriendRequest(user: User, userToReject: User, collection: str = "Users") -> bool:
+        # check if userToReject in DB
+        isPresent = UserHelpers.CheckUserPrescenceInDB(userToReject, collection)
+        if not isPresent:
+            print("Receiving user is not a registered user. Friend request can't be sent")
+            return False
+
+        # check if userToAdd in users friend list
+        if userToReject.Username not in user.Friends:
+            print(f"\nUh Oh! Something went wrong. {userToReject.Username} couldn't be found in your friends\n")
+            return False
+        # check if userToReject iis already accepted
+        elif user.Friends[userToReject.Username] == True:
+            print(f"Can't reject {userToReject.Username}. {userToReject.Username} is an accepted friend!\n")
+            return False
+
+        try:
+            #  remove userToRejects username from user's friends dict in db
+            del user.Friends[userToReject.Username]
+            new_dict = user.Friends
+            database.child(collection).child(user.Id).child("Friends").set(new_dict)
+            print(f"\nYou rejected {userToReject.Username}'s friend request.\n")
+            return True
+        except:
+            print(f"\nUh Oh, there seemed to be an error rejecting {userToReject.Username}'s request.\n")
+            return False
+
+
+    def DeleteFriend(user: User, userToDelete: User, collection: str = "Users") -> bool:
+
+        isPresent = UserHelpers.CheckUserPrescenceInDB(userToDelete, collection)
+        if not isPresent:
+            print("Receiving user is not a registered user. Friend request can't be sent")
+            return False
+        # check if user to delete is a friend
+        if userToDelete.Username not in user.Friends:
+            print(f"\n{userToDelete.Username} is not a friend of yours!\n")
+            return False
+        # check if user to delete is still on pending
+        elif user.Friends[userToDelete.Username] == False:
+            print(f"\n{userToDelete.Username}'s request is still pending!\n")
+            return False
+
+        try:
+            del user.Friends[userToDelete.Username]
+            new_dict = user.Friends
+            database.child(collection).child(user.Id).child("Friends").set(new_dict)
+        except:
+            print(f"\nUh Oh! There seemed to be an issue with removing {userToDelete.Username}\n")
+            return False
+
+
+
+
+
+
+
+
+
+
+        return True
+
 
 
