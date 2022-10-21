224b1790d9fd0840e3f1c5e5c74feaf26fd60908
obasit7 <basitosama@gmail.com>
2022-10-20 20:38:18 -0400

    added Profile class and useful methods

---
+++ source/model/Profile.py
@@ -0,0 +1,64 @@
+from dataclasses import dataclass, field
+#from firebaseSetup.Firebase import database
+import hashlib
+
+@dataclass
+class JobExperience:
+    ExperienceTitle: str = ""
+    Employer: str = ""
+    DateStarted: str = "" # MM/DD/YY format
+    DateEnded: str = "" # MM/DD/YY format
+    Location: str = ""
+    Description: str = ""
+
+    def ExpToDict(self):
+        return{
+            'Title': str(self.ExperienceTitle),
+            'Employer': str(self.Employer),
+            'DateStarted': str(self.DateStarted),
+            'DateEnded': str(self.DateEnded),
+            'Location': str(self.Location),
+            'Description': str(self.Description)
+        }
+
+@dataclass
+class Education:
+    SchoolName: str = ""
+    Degree: str = ""
+    YearsAttended: int = 0
+    def EducationToDict(self):
+        return {
+            'SchoolName': str(self.SchoolName),
+            'Degree': str(self.Degree),
+            'YearsAttended': str(self.YearsAttended)
+        }
+
+@dataclass
+class Profile:
+    Title: str = "" #e.g: Third year Comp Sci
+    University: str = "" # to grab from User as default
+    Major: str = ""  # to grab from User as default
+    About: str = "" # paragraph
+    ProfileEducation: Education = Education()
+    ProfileExperience: list[JobExperience] = field(default_factory=list)
+
+    def ProfileToDict(self):
+        return {
+            'Title': str(self.Title),
+            'University': str(self.University),
+            'Major': str(self.Major),
+            'About': str(self.About),
+            'Education': self.ProfileEducation.EducationToDict(),
+            'Experience': {i: self.ProfileExperience[i].ExpToDict() for i in range(len(self.ProfileExperience))}
+        }
+
+#Profile is of the following format
+# Profile(Title='', University='', Major='', About='',
+# ProfileEducation=Education(SchoolName='', Degree='', YearsAttended=0), ProfileExperience=Experience(PastJobs=[]))
+
+p = Profile()
+p.ProfileExperience.append(JobExperience())
+p.ProfileExperience.append(JobExperience())
+print(p.ProfileToDict())
+
+
