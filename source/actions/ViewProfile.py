25657bcfeb0faaf0bde85379e136083c829c691b
sid-ak <sidharthak25@gmail.com>
2022-10-27 01:24:32 -0400

    Cleaned up ViewProfile.

--- source/actions/ViewProfile.py
+++ source/actions/ViewProfile.py
@@ -1,59 +1,80 @@
-from model.User import User, Profile
-from firebaseSetup.Firebase import database
 from helpers.UserHelpers import UserHelpers
+from model.User import User
+from model.Profile import Education, Experience
 
 
 # non-return function to print the profile of a user
 def ViewProfile(loggedUser: User, userToSearch: User = None):
 
-    if not userToSearch:
-        printProfile(loggedUser)
+    if userToSearch == None:
+        PrintProfile(loggedUser)
     else:
-        printProfile(userToSearch)
+        PrintProfile(userToSearch)
 
 
-def printProfile(user: User):
+# Prints a profile.
+def PrintProfile(user: User):
     try:
-        print(f"\n{user.FirstName} {user.LastName}")
-        print("Title: " + user.Profile.Title)
-        print("University: " + user.Profile.University)
-        print("Major: " + user.Profile.Major)
-        print("About: " + user.Profile.About)
 
-        educationList = user.Profile.EducationList
-        if len(educationList) == 0:
-            print("Education: No education to show!")
-        else:
-            try:
-                EduIndex = 1
-                for education in user.Profile.EducationList:
-                    print(f"Education #{EduIndex}:")
-                    print(f"\tSchool: {'N/A' if education.SchoolName == '' else education.SchoolName}")
-                    print(f"\tDegree: {'N/A' if education.Degree == '' else education.Degree}")
-                    print(f"\tYears Attended: {'0' if education.YearsAttended == 0 else education.YearsAttended}\n")
-                    EduIndex += 1
-            except Exception as e:
-                print(f"Something went wrong while showing the education for user {user.Username}\n{e}")
+        # Get user from DB so saved changes are reflected.
+        user = UserHelpers.GetUserById(user.Id)
 
-        experienceList = user.Profile.ExperienceList
-        if len(experienceList) == 0:
-            print("Experience: No experience to show!")
-        else:
-            try:
-                ExpIndex = 1
-                for experience in experienceList:
-                    print(f"\tExperience #{ExpIndex}:")
-                    print(f"\tTitle: {experience.Title}")
-                    print(f"\tEmployer: {experience.Employer}")
-                    print(f"\tLocation: {experience.Location}")
-                    print(f"\tDate Started: {experience.DateStarted}")
-                    print(f"\tDate Ended: {experience.DateEnded}")
-                    print(f"\tDescription: {experience.Description}\n")
-                    ExpIndex += 1
-            except Exception as e:
-                print(f"Something went wrong while showing the experience for user {user.Username}\n{e}")
+        print(f"\n========== {user.FirstName} {user.LastName} ==========\n")
+
+        # Print Profile Details.
+        print(f"\nPrimary Details:"
+            + f"\n\tTitle: {user.Profile.Title}"
+            + f"\n\tUniversity: {user.Profile.University}"
+            + f"\n\tMajor: {user.Profile.Major}"
+            + f"\n\tAbout: {user.Profile.About}\n")
+
+        print(f"\n========== {user.FirstName}'s EDUCATION ==========\n")
+
+        # Print Education List.
+        PrintEducationList(user.Profile.EducationList)
+
+        print(f"\n========== {user.FirstName}'s EXPERIENCE ==========\n")
+        
+        # Print Experience List.
+        PrintExperienceList(user.Profile.ExperienceList)
+
+        print("\n========================================\n")
+    
     except Exception as e:
-        print(f"Something went wrong while showing the profile for user {user.Username}\n{e}")
+        print(f"Something went wrong while showing the profile for user {user.Username}\n{e}\n")
 
-    print()
 
+# Prints the list of educations under the profile of a user.
+def PrintEducationList(educationList: list[Education]):
+    if educationList == None or educationList == []:
+        print("Education: No education to show.")
+    else:
+        try:
+            eduIndex = 1
+            for education in educationList:
+                print(f"Education #{eduIndex}:"
+                    + f"\n\tSchool: {education.SchoolName}"
+                    + f"\n\tDegree: {education.Degree}"
+                    + f"\n\tYears Attended: {education.YearsAttended}\n")
+                eduIndex += 1
+        except Exception as e:
+            raise Exception(f"Something went wrong while showing education.\n{e}\n")
+
+
+# Prints the list of experiences under the profile of a user.
+def PrintExperienceList(experienceList: list[Experience]):
+    if experienceList == None or experienceList == []:
+        print("Experience: No experience to show.")
+    else:
+        try:
+            expIndex = 1
+            for experience in experienceList:
+                print(f"Experience #{expIndex}:"
+                    + f"\n\tTitle: {experience.Title}"
+                    + f"\n\tEmployer: {experience.Employer}"
+                    + f"\n\tLocation: {experience.Location}"
+                    + f"\n\tDate Started: {experience.DateStarted}"
+                    + f"\n\tDescription: {experience.Description}\n")
+                expIndex += 1
+        except Exception as e:
+            raise Exception(f"Something went wrong while showing experience.\n{e}\n")
