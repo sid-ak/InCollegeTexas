0ddf339bc5c5020968d5da71b09b72b16b2ef796
Aoun1207 <81834802+Aoun1207@users.noreply.github.com>
2022-11-03 14:01:56 -0400

    Update DisplayAllUser.py
    IN 68: added an option to sent a message to everyone if the user is a plus member

--- source/actions/DisplayAllUser.py
+++ source/actions/DisplayAllUser.py
@@ -1,11 +1,13 @@
 from model.User import User
 from helpers.UserHelpers import User, UserHelpers
 from helpers.MenuHelpers import MenuHelpers
+from helpers.MessageHelpers import MessageHelpers
+from model.Message import Message
  
 def DisplayEveryUser(loggedUser: User, collection: str = "Users"):
         allUser = UserHelpers.GetAllUsers(collection)
-        allUsers = list(map(lambda x: x.Username, allUser))
-        displayU = list(filter(lambda user: user != loggedUser.Username, allUsers))
+        allUsers = list(filter(lambda user: user.Username != loggedUser.Username, allUser))
+        displayU = list(map(lambda x: x.Username, allUsers))
         
         while True:
             print("\nSelect one of the user to send a message\n")
@@ -16,8 +18,18 @@ def DisplayEveryUser(loggedUser: User, collection: str = "Users"):
 
                 if optionNo == -1: break
 
-                elif optionNo in range(1, len(displayU) + 1):
-                    print("hi")
+                elif optionNo in range(1, len(allUser) + 1):
+                    content: str = str(input(f"\nEnter the content for the message to {displayU[optionNo - 1]}:\n\n"))
+
+                    messageSent: bool = MessageHelpers.UpdateMessage(Message(
+                        MessageHelpers.CreateMessageId(),
+                        loggedUser.Id,
+                        allUsers[optionNo - 1].Id,
+                        content
+                    ))
+
+                    if messageSent: print(f"\nMessage sent to {displayU[optionNo - 1]} successfully.")
+                    else: print("\nMessage was not sent.\n")
             except:
                 print("None")
 
