aa58404341151019f3603ae12a3abd0a33f37f21
sid-ak <sidharthak25@gmail.com>
2022-11-06 13:49:12 -0500

    IN-69: Cleaned up to use the new SendMessage().

--- source/actions/DisplayAllUser.py
+++ source/actions/DisplayAllUser.py
@@ -2,42 +2,46 @@ from model.User import User
 from helpers.UserHelpers import User, UserHelpers
 from helpers.MenuHelpers import MenuHelpers
 from helpers.MessageHelpers import MessageHelpers
-from model.Message import Message
 from enums.UserTierEnum import UserTierEnum
 
-#This function helps dislay all the user in the database and gives the user to send a message to anyone if they are plus member or are friends with them
-def DisplayEveryUser(loggedUser: User, collection: str = "Users"):
-        allUser = UserHelpers.GetAllUsers(collection)
-        allUsers = list(filter(lambda user: user.Username != loggedUser.Username, allUser))
-        displayU = list(map(lambda x: x.Username, allUsers))
+#This function helps display all the user in the database and gives the user to 
+# send a message to anyone if they are plus member or are friends with them
+def DisplayEveryUser(
+    loggedUser: User,
+    messageCollection: str = "Messages",
+    userCollection: str = "Users"):
+        
+        allUsers: list[User] = UserHelpers.GetAllUsers(userCollection)
+        otherUsers: list[User] = list(filter(lambda user: user.Username != loggedUser.Username, allUsers))
+        otherUsernames: list[str] = list(map(lambda x: x.Username, otherUsers))
 
         while True:
             
             print("\nSelect one of the user to send a message\n")
-            MenuHelpers.DisplayOptions(displayU)
+            MenuHelpers.DisplayOptions(otherUsernames)
 
             try:
                 optionNo: int = MenuHelpers.InputOptionNo()
-                isFriends:bool = loggedUser.Friends.get(displayU[optionNo - 1])
+                friendUsername: str = otherUsernames[optionNo - 1]
+                isFriends :bool = loggedUser.Friends.get(friendUsername)
+                friendUserId: User = otherUsers[optionNo - 1].Id
 
                 if optionNo == -1: break
 
-                elif optionNo in range(1, len(allUser) + 1):
-                    if(loggedUser.TierEnum == UserTierEnum.Plus or isFriends):
-                        content: str = str(input(f"\nEnter the content for the message to {displayU[optionNo - 1]}:\n\n"))
-
-                        messageSent: bool = MessageHelpers.UpdateMessage(Message(
-                            MessageHelpers.CreateMessageId(),
+                elif optionNo in range(1, len(allUsers) + 1):
+                    
+                    if (loggedUser.TierEnum == UserTierEnum.Plus or isFriends):
+                        messageSent: bool = MessageHelpers.SendMessage(
                             loggedUser.Id,
-                            allUsers[optionNo - 1].Id,
-                            content
-                        ))
+                            friendUserId,
+                            messageCollection,
+                            userCollection)
+
+                        if not messageSent: raise Exception()
+                    
                     else:
                         print("\nI'm sorry, you are not friends with that person.\n")
                         continue
 
-                    if messageSent: print(f"\nMessage sent to {displayU[optionNo - 1]} successfully.")
-                    else: print("\nMessage was not sent.\n")
             except Exception as e:
                 raise Exception(f"Something went wrong, could not display all the users\n{e}")
-            
