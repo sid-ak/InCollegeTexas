86778fabf9015d3b20ec74be3168bb0b89aa6788
sid-ak <sidharthak25@gmail.com>
2022-11-08 00:02:38 -0500

    IN-73: Displayed the notification if a user has not applied to any job.

--- source/helpers/JobNotificationHelpers.py
+++ source/helpers/JobNotificationHelpers.py
@@ -9,8 +9,13 @@ class JobNotificationHelpers:
     # Notifies the user if they did not apply for a job for a certain period     of time.
     def NotifyIfNotAppliedJob(loggedUser: User, collection = "AppliedJobs"):
         
+        notificationStr: str = "\nRemember - you're going to want to have a job when you graduate."
+        notificationStr += "\nMake sure that you start to apply for jobs today!\n"
+
         lastAppliedJob: AppliedJob = AppliedJobHelpers.GetLastAppliedJob(loggedUser, collection)
-        if lastAppliedJob == None: return
+        if lastAppliedJob == None:
+            print(notificationStr)
+            return
 
         lastAppliedJobTimestamp: datetime = lastAppliedJob._CreatedTimestamp
         currentTimestamp: datetime = datetime.now()
@@ -19,6 +24,4 @@ class JobNotificationHelpers:
         timeElapsed: timedelta = currentTimestamp - lastAppliedJobTimestamp
         daysElapsed: int = timeElapsed.days
 
-        if daysElapsed >= maxDays:
-            print("\nRemember - you're going to want to have a job when you graduate."
-                + "\nMake sure that you start to apply for jobs today!\n")
+        if daysElapsed >= maxDays: print(notificationStr)
