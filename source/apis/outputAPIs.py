036d9631efcbfdca0ebc8bf1165a6facf29f3ade
anshika-bhowmick <anshikabhowmick5@gmail.com>
2022-11-17 01:31:14 -0500

    worked on both IN-90 and IN-94

--- source/apis/outputAPIs.py
+++ source/apis/outputAPIs.py
@@ -8,11 +8,18 @@ from helpers.JobHelpers import JobHelpers
 from helpers.AppliedJobHelpers import AppliedJobHelpers
 from helpers.UserHelpers import UserHelpers
 from helpers.APIHelpers import getCurrentPath
+from helpers.ProfileHelpers import ProfileHelpers
 
 def RunOutputAPIs(userCollection: str = "Users", jobsCollection: str = "Jobs", appliedJobsCollection:str = "AppliedJobs") -> bool:
     try:
         if not AppliedJobsAPI(userCollection, jobsCollection, appliedJobsCollection):
             raise Exception("Applied Jobs API failed")
+
+        if not UserAPI(userCollection):
+            raise Exception("User API failed")
+
+        if not UserProfileAPI():
+            raise Exception("User Profiles API failed")
 
         return True
     except Exception as e:
@@ -64,3 +71,59 @@ def AppliedJobsAPI(userCollection: str = "Users", jobsCollection: str = "Jobs", 
         print(f"An error occurred running the applied jobs output API! {e}\n")
         return False
 
+# User API: write to file all users and their subscriptions
+def UserAPI(userCollection:str = "Users", subscriptionCollection:str = "Subscriptions") -> bool:
+    # create file called MyCollege_users.txt
+    try: FILE_NAME = os.path.join(getCurrentPath(), "MyCollege_users.txt")
+    except Exception as e:
+        print(f"Error appending file name to path {e}\n")
+        return False
+
+    try:
+        with open(FILE_NAME, "w") as outputFile:
+            allUsers = UserHelpers.GetAllUsers(collection=userCollection)
+            if allUsers == None: raise Exception("Users couldn't be retrieved")
+            for user in allUsers:
+                # place username of each user
+                try: outputFile.write("{}, {}\n".format(user.Username, str(user.TierEnum).lstrip("UserTierEnum.")))
+                except Exception as e: print(f"Could not write to file {e}\n")
+
+        return True
+
+    except Exception as e:
+        print(f"An error occurred running the user output API! {e}\n")
+        return False
+
+# User Profiles API: write to file all users and their profiles
+def UserProfileAPI(userCollection:str = "Users") -> bool:
+    # create file called MyCollege_profiles.txt
+    try: FILE_NAME = os.path.join(getCurrentPath(), "MyCollege_profiles.txt")
+    except Exception as e:
+        print(f"Error appending file name to path {e}\n")
+        return False
+
+    try:
+        with open(FILE_NAME, "w") as outputFile:
+            allUsers = UserHelpers.GetAllUsers(collection=userCollection)
+            if allUsers == None: raise Exception("Users couldn't be retrieved")
+            for user in allUsers:
+                # get user profile
+                userProfile = user.Profile
+                if userProfile == None: raise Exception("User profile couldn't be retrieved")
+
+                if ProfileHelpers.ProfileExists(user):
+                    # write user profile to file
+                    try: outputFile.write(f"{userProfile.Title}\n{userProfile.Major}\n{userProfile.University}\n{userProfile.About}\n{userProfile.ExperienceList}\n{userProfile.EducationList}\n")
+                    except Exception as e: print(f"Could not write to file {e}\n")
+
+                    # each user profile separated by a line of "====="
+                    try:
+                        outputFile.write("=====\n")
+                    except Exception as e:
+                        print(f"Error writing to file {e}\n")
+
+        return True
+
+    except Exception as e:
+        print(f"An error occurred running the user profile output API! {e}\n")
+        return False
